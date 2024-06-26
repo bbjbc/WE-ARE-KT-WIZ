aeed595dd45f883b30271b79906183b9ba1d1058
bbjbc <jobc4550@gmail.com>
2024-06-26 22:50:40 +0900

    feat: 개발 환경 세팅

---
+++ kt-wiz-app/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="m-10 flex items-center justify-center rounded-lg bg-slate-300 p-4 text-2xl font-semibold underline">
+      Kt Wiz 마법은 영원히
+    </div>
+  );
+};
+
+export default App;
