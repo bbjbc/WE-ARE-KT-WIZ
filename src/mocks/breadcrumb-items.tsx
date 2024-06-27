82ec6fe99d2e9be1676dc65022fa740bae252d6a
bbjbc <jobc4550@gmail.com>
2024-06-28 00:55:01 +0900

    refactor: breadcrumb 아이템 추가

--- src/mocks/breadcrumb-items.tsx
+++ src/mocks/breadcrumb-items.tsx
@@ -4,4 +4,7 @@ export const BreadcrumbItems = [
   { label: 'kt wiz 응원단', link: '/cheer-squad' },
   { label: 'kt wiz 선수단', link: '/players' },
   { label: '경기 예매 방법', link: '/tickets' },
+  { label: '코칭스텝', link: '/players/coach' },
+  { label: '투수', link: '/players/pitcher' },
+  { label: '타자', link: '/players/catcher' },
 ];
