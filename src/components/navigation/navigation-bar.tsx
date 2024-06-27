8453d10866dc2952f3cfd769d8dc51ad11857b5e
bbjbc <jobc4550@gmail.com>
2024-06-27 17:29:09 +0900

    feat: 레이아웃 및 네비게이션바 추가

---
+++ src/components/navigation/navigation-bar.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+import DropdownMenu from './dropdown-menu';
+
+const NavigationBar = () => {
+  return (
+    <div className="relative z-30 flex justify-between bg-slate-950 bg-opacity-90 px-4 py-2 align-top blur-none">
+      <Link to="/">
+        <h1 className="mx-2 px-2 font-extrabold text-white transition-all duration-200 hover:scale-105">
+          KT WIZ
+        </h1>
+      </Link>
+      <DropdownMenu />
+    </div>
+  );
+};
+
+export default NavigationBar;
--- src/router/Layout.tsx
+++ src/router/Layout.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router-dom';
 
+import NavigationBar from '../components/navigation/navigation-bar';
+
 const Layout = () => {
   return (
     <main className="flex items-center">
       <section className="scrollbar-hidden mx-auto min-h-screen w-[100vw] max-w-[450px] overflow-x-hidden bg-white">
+        <NavigationBar />
         <Outlet />
       </section>
     </main>
