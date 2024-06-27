a55af0af90ae0cdfc4005c28134df0bd2a6834a8
bbjbc <jobc4550@gmail.com>
2024-06-27 21:55:43 +0900

    feat: introduction 내용 추가 및 푸터 추가

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -1,7 +1,9 @@
 import Header from '../components/header/header';
 import ktIntroImg from '../assets/ktIntroImg.png';
 import ktHeaderImg from '../assets/ktHeaderImg.png';
+import ktIntroBottomImg from '../assets/ktIntroBottomImg.png';
 import ContentLayout from '../components/content/content-layout';
+import IntroductionContent from '../components/content/introduction-content';
 
 const IntroductionPage = () => {
   return (
@@ -11,9 +13,15 @@ const IntroductionPage = () => {
         title="kt wiz는?"
         description="한국 프로야구의 '10번째 심장' kt wiz를 소개합니다."
       />
-      <ContentLayout>
-        <img src={ktIntroImg} alt="kt wiz" className="w-full" />
-      </ContentLayout>
+      <main>
+        <ContentLayout>
+          <img src={ktIntroImg} alt="kt wiz" className="w-full rounded-xl" />
+          <IntroductionContent />
+        </ContentLayout>
+      </main>
+      <footer>
+        <img src={ktIntroBottomImg} alt="kt wiz" className="w-full" />
+      </footer>
     </>
   );
 };
