ab0a23f1ba197ccc23d65ebec5f744ade68913c6
bbjbc <jobc4550@gmail.com>
2024-06-27 20:40:43 +0900

    feat: breadcrumb item 목데이터 추가

---
+++ src/mocks/breadcrumb-items.tsx
@@ -0,0 +1,7 @@
+export const BreadcrumbItems = [
+  { label: '홈페이지', link: '/' },
+  { label: 'kt wiz 소개', link: '/introduction' },
+  { label: 'kt wiz 응원단', link: '/cheer-squad' },
+  { label: 'kt wiz 선수단', link: '/players' },
+  { label: '경기 예매 방법', link: '/tickets' },
+];
