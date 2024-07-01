63641e8fdd1fe2d6f93bc421224d2480a66bdae0
bbjbc <jobc4550@gmail.com>
2024-07-01 23:36:46 +0900

    feat: gongGothic 글씨체 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        gonggothic: ['GongGothicMedium', 'GmarketSansMedium'],
+      },
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 };
