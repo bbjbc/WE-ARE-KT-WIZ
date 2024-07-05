aaa10b33f1bb3081891a8a53086117c1345490c0
bbjbc <jobc4550@gmail.com>
2024-07-06 00:27:05 +0900

    refactor: 팀 응원가도 검색창 추가로 조건부 렌더링

--- src/components/song/team-song-list.tsx
+++ src/components/song/team-song-list.tsx
@@ -1,3 +1,7 @@
+import { useAtomValue } from 'jotai';
+import { chosungIncludes } from 'es-hangul';
+
+import { searchTermAtom } from '../../utils/atoms';
 import TeamSongItem from './team-song-item';
 
 interface TeamSong {
@@ -12,13 +16,21 @@ interface TeamSongListProps {
 }
 
 const TeamSongList: React.FC<TeamSongListProps> = ({ teamSongs }) => {
+  const searchTerm = useAtomValue(searchTermAtom);
+
+  const filteredTeamSongs = teamSongs.filter(
+    (teamSong) =>
+      teamSong.name.includes(searchTerm) ||
+      chosungIncludes(teamSong.name, searchTerm),
+  );
+
   return (
     <article className="mb-6 mt-2">
       <h1 className="mb-2 flex justify-center text-sm">
         kt wiz의 팀 응원가 리스트입니다.
       </h1>
 
-      {teamSongs.map((teamSong) => (
+      {filteredTeamSongs.map((teamSong) => (
         <div key={teamSong.id}>
           <ul className="flex flex-col">
             <li key={teamSong.id} className="my-2">
