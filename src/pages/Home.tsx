7cc74b75ae83925cbfb3c399407f124bfec26fd7
bbjbc <jobc4550@gmail.com>
2024-07-07 21:19:39 +0900

    feat: 홈페이지 메인 이미지, 무한 슬라이더, 텍스트타이포 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,47 @@
+import { motion } from 'framer-motion';
+
+import ktRemoveBgImg from '../assets/ktLogo-removebg-preview.png';
+import ktHomeBgImg from '../assets/ktHomeBgImg.png';
+import TextType from '../components/ui/typo/text-type';
+import InfiniteSlider from '../components/ui/slider/infinite-slider';
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <article
+      style={{ backgroundImage: `url(${ktHomeBgImg})` }}
+      className="flex h-screen flex-col gap-8 bg-cover bg-center py-6"
+    >
+      <motion.img
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, type: 'spring', stiffness: 400 }}
+        whileHover={{ scale: 1.04 }}
+        whileTap={{ scale: 0.9 }}
+        src={ktRemoveBgImg}
+        alt="ktLogo"
+        className="mb-2 w-full"
+      />
+      <TextType text={'WE ARE GREAT MAGIC\n WINNING KT!'} />
+
+      <InfiniteSlider
+        topItems={[
+          'KBO 신생팀 중 최단 기간 우승',
+          '우리가 만드는 승리의 순간',
+          '수원 kt 위즈 파크',
+          '함께 가는 kt wiz, 언제나 승리!',
+          '날려버려',
+        ]}
+        bottomItems={[
+          'SUWON',
+          'WIZARD',
+          'MIRACLE',
+          'WINNING KT!',
+          'HISTORY',
+          'FOREVER',
+          'TOGETHER',
+        ]}
+      />
+    </article>
   );
 };
 
