58a56aa4aec23b38b3b122a680838f7550ddd466
bbjbc <jobc4550@gmail.com>
2024-07-07 20:55:57 +0900

    style: 왼쪽, 오른쪽 그라데이션 효과 추가

--- src/components/ui/slider/slider-row.tsx
+++ src/components/ui/slider/slider-row.tsx
@@ -11,6 +11,12 @@ const SliderRow: React.FC<SliderRowProps> = ({ items, direction }) => {
 
   return (
     <article className="relative overflow-hidden whitespace-nowrap py-3">
+      {/* 왼쪽 부분 그라데이션 */}
+      <div className="pointer-events-none absolute bottom-0 left-0 top-0 z-10 w-10 bg-gradient-to-r from-sky-600 to-transparent" />
+
+      {/* 오른쪽 부분 그라데이션 */}
+      <div className="pointer-events-none absolute bottom-0 right-0 top-0 z-10 w-10 bg-gradient-to-l from-red-600 to-transparent" />
+
       <motion.div
         className="flex"
         initial={{ x: direction === 'left' ? '0%' : '-100%' }}
