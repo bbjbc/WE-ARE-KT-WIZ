7906813175e886fc9453127e4a92842602bc696a
bbjbc <jobc4550@gmail.com>
2024-07-10 16:42:44 +0900

    refactor: 필터링되는 결과가 적을 경우 footer 올라오는 경우로 인한 수정

--- src/router/Layout.tsx
+++ src/router/Layout.tsx
@@ -6,16 +6,16 @@ import Footer from '../components/footer/footer';
 
 const Layout = () => {
   return (
-    <>
-      <main className="flex h-screen w-screen items-center">
-        <section className="scrollable-section relative mx-auto h-full w-full max-w-[450px] overflow-y-auto overflow-x-hidden bg-white scrollbar-hide">
-          <NavigationBar />
+    <main className="flex h-screen w-screen items-center justify-center">
+      <section className="scrollable-section relative flex h-full w-full max-w-[450px] flex-col overflow-y-auto overflow-x-hidden bg-white scrollbar-hide">
+        <NavigationBar />
+        <div className="flex-grow">
           <Outlet />
-          <ScrollToTopButton />
-          <Footer />
-        </section>
-      </main>
-    </>
+        </div>
+        <Footer />
+        <ScrollToTopButton />
+      </section>
+    </main>
   );
 };
 
